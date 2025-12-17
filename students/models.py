66a813d7441725efed842be8f3ba04907269bb7b
Your GitHub Username <brightfutureskills08@gmail.com>
2025-12-18 00:09:35 +0300

    error correction

--- students/models.py
+++ students/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Student(models.Model):
-    registration_number = models.CharField(max_length=50, unique=True, verbose_name="Namba ya Usajili")
-    full_name = models.CharField(max_length=255, unique=True, verbose_name="Jina Kamili")
-    phone_number = models.CharField(max_length=15, null=True, verbose_name="Namba ya Simu")
+    registration_number = models.CharField(max_length=50, unique=True, verbose_name="Registration Number")
+    full_name = models.CharField(max_length=255, unique=True, verbose_name="Full Name")
+    phone_number = models.CharField(max_length=15, null=True, verbose_name="Phone Number")
     is_registered = models.BooleanField(default=False)
 
     def __str__(self):
